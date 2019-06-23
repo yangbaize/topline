0a511faacd11addb2d1c4c9ffabbdb27cb84c936
yangbaize <ybz117000@163.com>
2019-06-23 17:40:29 +0800

    完成验证码定时器

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,21 +4,25 @@
       <div class="form-head">
         <img src="./logo_index.png" alt="黑马头条号">
       </div>
-      <el-form class="form-content" ref="form" :model="form">
-        <el-form-item>
+      <el-form class="form-content" ref="form" :model="form" :rules="rules">
+        <el-form-item prop="mobile">
           <el-input v-model="form.mobile" placeholder="手机号"></el-input>
         </el-form-item>
-        <el-form-item>
 
+        <el-form-item prop="code">
           <el-col :span="14">
             <el-input v-model="form.code" placeholder="验证码"></el-input>
           </el-col>
-
           <el-col :offset="1" :span="9">
-            <el-button @click="handleSandCode">获取验证码</el-button>
+            <el-button @click="handleSandCode" :disabled="!!codeTimer">{{ codeTimer ? `剩余${codeTimerSeconds}秒` : '获取验证码' }}</el-button>
           </el-col>
-
         </el-form-item>
+
+        <el-form-item prop="agree">
+          <el-checkbox class="agree-checkbox" v-model="form.agree"></el-checkbox>
+          <span class="agree-text">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a></span>
+        </el-form-item>
+
         <el-form-item>
           <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
         </el-form-item>
@@ -30,18 +34,45 @@
 <script>
 import axios from 'axios'
 import '@/vendor/gt'
+const initCodeTimerSeconds = 10
+
 export default {
   name: 'AppLogin',
   data () {
     return {
       form: {
         mobile: '',
-        code: ''
-      }
+        code: '',
+        agree: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { len: 11, message: '长度11个字符', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '长度6个字符', trigger: 'blur' }
+        ],
+        agree: [
+          { required: true, message: '请同意用户协议', trigger: 'blur' },
+          { pattern: /true/, message: '请同意用户协议', trigger: 'blur' }
+        ]
+      },
+      codeTimer: null,
+      codeTimerSeconds: initCodeTimerSeconds
     }
   },
   methods: {
     handleLogin () {
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
@@ -61,6 +92,14 @@ export default {
         })
     },
     handleSandCode () {
+      this.$refs['form'].validateField('mobile', errorMessage => {
+        if (errorMessage.trim().length > 0) {
+          return
+        }
+        this.showGeetest()
+      })
+    },
+    showGeetest () {
       const { mobile } = this.form
       axios({
         method: 'GET',
@@ -73,10 +112,10 @@ export default {
           offline: !data.success,
           new_captcha: data.new_captcha,
           product: 'bind'
-        }, function (captchaObj) {
-          captchaObj.onReady(function () {
+        }, captchaObj => {
+          captchaObj.onReady(() => {
             captchaObj.verify()
-          }).onSuccess(function () {
+          }).onSuccess(() => {
             const {
               geetest_challenge: challenge,
               geetest_validate: validate,
@@ -91,13 +130,23 @@ export default {
                 seccode
               }
             }).then(res => {
-              console.log(res.data)
+              this.codeCountDown()
             })
             console.log(captchaObj.getValidate())
           }).onError(function () {
           })
         })
       })
+    },
+    codeCountDown () {
+      this.codeTimer = window.setInterval(() => {
+        this.codeTimerSeconds--
+        if (this.codeTimerSeconds <= 0) {
+          window.clearInterval(this.codeTimer)
+          this.codeTimerSeconds = initCodeTimerSeconds
+          this.codeTimer = null
+        }
+      }, 1000)
     }
   }
 }
@@ -106,7 +155,9 @@ export default {
 <style lang="less" scoped>
 .login-wrap {
   height: 100%;
-  background: url("./login_bg.jpg");
+  width: 100%;
+  // background: url("./login_bg.jpg");
+  background-image: url("./login_bg.jpg");
   display: flex;
   justify-content: center;
   align-items: center;
